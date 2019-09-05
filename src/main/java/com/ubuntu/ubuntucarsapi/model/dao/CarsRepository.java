5913be3ceca762834a8bc05287d677b4e250ae74
Pablo Plasencia <p.plasencia@preving.com>
2019-09-05 09:09:29 +0200

    first commit

--- src/main/java/com/ubuntu/ubuntucarsapi/model/dao/CarsRepository.java
+++
@@ -1,6 +0,0 @@
-package com.ubuntu.ubuntucarsapi.model.dao;
-
-import com.ubuntu.ubuntucarsapi.model.domain.Car;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface CarsRepository extends PagingAndSortingRepository<Car, Integer> {}
